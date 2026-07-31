671844a8a78bec6fac53c79634032ccfee1550cd
signalasi <signalasi@hotmail.com>
2026-07-31 08:09:29 +0800

    Add iOS phone native runtime factory (#486)

--- apps/ios/SignalASI/AgentPhoneNativeToolCatalogDefaults.swift
+++ apps/ios/SignalASI/AgentPhoneNativeToolCatalogDefaults.swift
@@ -1,6 +1,82 @@
 import Foundation
 
+struct AgentPhoneNativeToolRuntime {
+  var registry: AgentNativeToolRegistry
+  var actionExecutor: AgentActionExecutor
+  var nativeActionExecutor: AgentNativeToolActionExecutor
+
+  init(
+    registry: AgentNativeToolRegistry,
+    actionExecutor: AgentActionExecutor,
+    nativeActionExecutor: AgentNativeToolActionExecutor
+  ) {
+    self.registry = registry
+    self.actionExecutor = actionExecutor
+    self.nativeActionExecutor = nativeActionExecutor
+  }
+}
+
 extension AgentPhoneNativeToolCatalog {
+  static func defaultRuntime(
+    workspaceStore: AgentWorkspaceNativeToolExecutor? = nil,
+    actionExecutor: AgentActionExecutor,
+    screenProvider: @escaping (AgentNativeToolInvocation) -> AgentScreenContext,
+    capabilityStatusProvider: @escaping () -> [AgentPhoneCapabilityStatus] = {
+      AgentPhoneCapabilityCatalog.declaredStatuses()
+    },
+    storageRootURL: URL = AgentNativeToolDefaultStorePaths.applicationSupportRootURL(),
+    fileManager: FileManager = .default,
+    nowMillis: @escaping () -> Int64 = {
+      Int64((Date().timeIntervalSince1970 * 1_000).rounded())
+    },
+    guardSideEffects: Bool = true,
+    homeAssistantSettingsProvider: @escaping () -> HomeAssistantSettings = { .default },
+    homeAssistantProvider: AgentIOSHomeAssistantToolProviding? = nil,
+    notificationProvider: AgentIOSNotificationToolProviding = AgentIOSUnavailableNotificationToolProvider(),
+    visibleCaptureProvider: AgentIOSVisibleCaptureToolProviding = AgentIOSUnavailableVisibleCaptureToolProvider(),
+    webMediaProvider: AgentIOSWebMediaToolProviding = AgentIOSUnavailableWebMediaToolProvider(),
+    webIntelligenceProvider: AgentIOSWebIntelligenceToolProviding = AgentIOSUnavailableWebIntelligenceToolProvider(),
+    mediaProvider: AgentIOSMediaNativeToolProviding = AgentIOSUnavailableMediaNativeToolProvider(),
+    selfEvolutionProvider: AgentIOSSelfEvolutionToolProviding = AgentIOSUnavailableSelfEvolutionToolProvider(),
+    desktopRemoteProvider: AgentIOSDesktopRemoteToolProviding = AgentIOSUnavailableDesktopRemoteToolProvider(),
+    mcpProvider: AgentIOSMcpNativeToolProviding = AgentIOSUnavailableMcpNativeToolProvider(),
+    onDeviceRuntimeProvider: AgentIOSOnDeviceRuntimeToolProviding = AgentIOSUnavailableOnDeviceRuntimeToolProvider()
+  ) throws -> AgentPhoneNativeToolRuntime {
+    let registry = try defaultRegistry(
+      workspaceStore: workspaceStore,
+      actionExecutor: actionExecutor,
+      screenProvider: screenProvider,
+      capabilityStatusProvider: capabilityStatusProvider,
+      storageRootURL: storageRootURL,
+      fileManager: fileManager,
+      nowMillis: nowMillis,
+      homeAssistantSettingsProvider: homeAssistantSettingsProvider,
+      homeAssistantProvider: homeAssistantProvider,
+      notificationProvider: notificationProvider,
+      visibleCaptureProvider: visibleCaptureProvider,
+      webMediaProvider: webMediaProvider,
+      webIntelligenceProvider: webIntelligenceProvider,
+      mediaProvider: mediaProvider,
+      selfEvolutionProvider: selfEvolutionProvider,
+      desktopRemoteProvider: desktopRemoteProvider,
+      mcpProvider: mcpProvider,
+      onDeviceRuntimeProvider: onDeviceRuntimeProvider
+    )
+    let nativeActionExecutor = AgentNativeToolActionExecutor(
+      registry: registry,
+      delegate: actionExecutor,
+      nowMillis: nowMillis
+    )
+    let resolvedActionExecutor: AgentActionExecutor = guardSideEffects
+      ? PhoneExecutionAuthority.guarded(nativeActionExecutor)
+      : nativeActionExecutor
+    return AgentPhoneNativeToolRuntime(
+      registry: registry,
+      actionExecutor: resolvedActionExecutor,
+      nativeActionExecutor: nativeActionExecutor
+    )
+  }
+
   static func defaultRegistry(
     workspaceStore: AgentWorkspaceNativeToolExecutor? = nil,
     actionExecutor: AgentActionExecutor,
