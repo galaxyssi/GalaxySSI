ea63c1c89df9b40e856148464222d65e811fe404
SignalASI <signalasi@hotmail.com>
2026-08-12 03:23:22 +0800

    Split iOS contact avatar view

--- apps/ios/SignalASI/SignalASIViews.swift
+++ apps/ios/SignalASI/SignalASIViews.swift
@@ -2448,65 +2448,6 @@ struct AddCloudModelView: View {
   }
 }
 
-struct AvatarView: View {
-  var contact: SignalASIContact
-  var size: CGFloat = 42
-
-  var body: some View {
-    ZStack {
-      if let assetName {
-        Image(assetName)
-          .resizable()
-          .scaledToFill()
-      } else {
-        Circle()
-          .fill(color)
-        Image(systemName: iconName)
-          .foregroundColor(.white)
-          .font(.system(size: max(14, size * 0.43), weight: .semibold))
-      }
-    }
-    .frame(width: size, height: size)
-    .clipShape(Circle())
-  }
-
-  private var assetName: String? {
-    let identityFields = [
-      contact.id,
-      contact.signalASIId,
-      contact.name,
-      contact.displayName,
-      contact.type
-    ]
-
-    if let agentAssetName = SignalASIAgentAvatarAssetCatalog.assetName(for: identityFields) {
-      return agentAssetName
-    }
-
-    let identity = identityFields.joined(separator: " ").lowercased()
-    if contact.deliveryMode.isSignalASILinkFamily || identity.contains("signalasi") {
-      return "SignalASILogo"
-    }
-    return nil
-  }
-
-  private var iconName: String {
-    switch contact.deliveryMode {
-    case .cloudAPI: return "cloud"
-    case .link, .pcConnector: return "desktopcomputer"
-    case .local: return "gearshape"
-    }
-  }
-
-  private var color: Color {
-    switch contact.deliveryMode {
-    case .cloudAPI: return .purple
-    case .link, .pcConnector: return .green
-    case .local: return .gray
-    }
-  }
-}
-
 struct FriendRequestRow: View {
   @Environment(\.signalASIInterfaceLanguage) private var interfaceLanguage
   var request: SignalASIFriendRequest
