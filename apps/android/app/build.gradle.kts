77e723cd67af157b6eaa1f8fcbb90aeccfdb952a
SignalASI <signalasi@hotmail.com>
2026-08-18 18:55:27 +0800

    Run phone project Git through Linux

--- apps/android/app/build.gradle.kts
+++ apps/android/app/build.gradle.kts
@@ -136,8 +136,8 @@ android {
         applicationId = "com.signalasi.chat"
         minSdk = 26
         targetSdk = 34
-        versionCode = 527
-        versionName = "0.4.127"
+        versionCode = 528
+        versionName = "0.4.128"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "WHISPER_NATIVE_VERSION", "\"v1.9.1-f049fff95a08\"")
         buildConfigField("String", "WHISPER_NATIVE_BUILD_FINGERPRINT", "\"$whisperNativeBuildFingerprint\"")
