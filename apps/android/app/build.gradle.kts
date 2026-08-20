ae98d5fc1570877c14407ae9939ea0fc1dddf0b9
SignalASI <signalasi@hotmail.com>
2026-08-20 15:03:33 +0800

    Allow cold Linux package index refresh

--- apps/android/app/build.gradle.kts
+++ apps/android/app/build.gradle.kts
@@ -139,8 +139,8 @@ android {
         applicationId = "com.signalasi.chat"
         minSdk = 26
         targetSdk = 34
-        versionCode = 542
-        versionName = "0.4.142"
+        versionCode = 543
+        versionName = "0.4.143"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "WHISPER_NATIVE_VERSION", "\"v1.9.1-f049fff95a08\"")
         buildConfigField("String", "WHISPER_NATIVE_BUILD_FINGERPRINT", "\"$whisperNativeBuildFingerprint\"")
