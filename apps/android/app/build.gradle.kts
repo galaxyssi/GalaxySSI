80c8e8f45c5d0974b12464d8c7c57a2ebfaf5844
SignalASI <signalasi@hotmail.com>
2026-08-24 12:39:34 +0800

    Deduplicate replan action snapshots

--- apps/android/app/build.gradle.kts
+++ apps/android/app/build.gradle.kts
@@ -139,8 +139,8 @@ android {
         applicationId = "com.signalasi.chat"
         minSdk = 26
         targetSdk = 34
-        versionCode = 667
-        versionName = "0.4.267"
+        versionCode = 668
+        versionName = "0.4.268"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "WHISPER_NATIVE_VERSION", "\"v1.9.1-f049fff95a08\"")
         buildConfigField("String", "WHISPER_NATIVE_BUILD_FINGERPRINT", "\"$whisperNativeBuildFingerprint\"")
